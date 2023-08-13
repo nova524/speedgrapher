6d61bb297f6008cbf0d551e656118d3447c94f53
supernova <seungmin10524@gmail.com>
2023-08-13 20:54:58 +0900

    logo

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import speedtest
 import matplotlib.pyplot as plt
 
+print('''                          _                       _                 __   ___  
+                         | |                     | |               /_ | / _ \ 
+  ___ _ __   ___  ___  __| | __ _ _ __ __ _ _ __ | |__   ___ _ __   | || | | |
+ / __| '_ \ / _ \/ _ \/ _` |/ _` | '__/ _` | '_ \| '_ \ / _ \ '__|  | || | | |
+ \__ \ |_) |  __/  __/ (_| | (_| | | | (_| | |_) | | | |  __/ |     | || |_| |
+ |___/ .__/ \___|\___|\__,_|\__, |_|  \__,_| .__/|_| |_|\___|_|     |_(_)___/ 
+     | |                     __/ |         | |                                
+     |_|                    |___/          |_|                                ''')
+
 def test_internet_speed():
     st = speedtest.Speedtest()
     st.get_best_server()
 
-    download_speed = st.download() / 10**6  # Mbps로 변환
-    upload_speed = st.upload() / 10**6  # Mbps로 변환
-
+    download_speed = st.download() / 10**6
+    upload_speed = st.upload() / 10**6
     return download_speed, upload_speed
 
 if __name__ == "__main__":
